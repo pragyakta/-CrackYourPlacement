72ed48be3309c04974fc12cbfaaf8202a182bcb6
pragyakta <107023407+pragyakta@users.noreply.github.com>
2024-08-03 16:51:31 +0530

    Time: 1 ms (80.67%), Space: 45.5 MB (42.41%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,25 +1,37 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[] dummy1 = new int[matrix.length];
-        int[] dummy2 = new int[matrix[0].length];
-        Arrays.fill(dummy1,-1); //for rows zeros
-        Arrays.fill(dummy2,-1); //for cols zeros
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dup = new int[n][m];
         
-        for(int row=0 ; row < matrix.length; row++){
-            for(int col=0 ; col < matrix[row].length; col++){
-                if(matrix[row][col]==0){  //if this element is zero
-                    dummy1[row]=0;       //set this index to zero
-                    dummy2[col]=0;        
-                }
-            }
-        }
-        for(int row=0 ; row < matrix.length; row++){
-            for(int col=0 ; col < matrix[row].length; col++ ){
-                if(dummy1[row]==0 || dummy2[col]==0){ //if any of the array's element on this index is zero
-                    matrix[row][col]=0; //set those index as zero
-                }
+        for(int i = 0; i< n;i++){
+            for(int j = 0 ;j < m;j++){
+                dup[i][j] = matrix[i][j];
             }
         }
         
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j < m;j++){
+                if(matrix[i][j] == 0){
+                    for(int k = j;k < m;k++){
+                        dup[i][k] = 0;
+                    }
+                    for(int k = j;k >= 0;k--){
+                        dup[i][k] = 0;
+                    }
+                    for(int k = i;k < n;k++){
+                        dup[k][j] = 0;
+                    }
+                    for(int k = i;k >= 0;k--){
+                        dup[k][j] = 0;
+                    }   
+                }
+            }
+        }
+        for(int i = 0; i< n;i++){
+            for(int j = 0 ;j < m;j++){
+                matrix[i][j] = dup[i][j] ;
+            }
+        }
     }
 }
